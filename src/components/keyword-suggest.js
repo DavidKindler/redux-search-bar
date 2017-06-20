5f1057c1b649c5124d6e6838fead9fcac719dc00
DavidKindler <kindlerdavid@gmail.com>
2017-06-20 11:25:09 -0500

    add comments

--- src/components/keyword-suggest.js
+++ src/components/keyword-suggest.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 import SuggestionList from '../containers/suggestion-list';
 import KeywordInput from '../containers/keyword-input'
 
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 
 class KeywordSuggest extends Component {
-
+    
     render() {
-        // let title = (this.props.suggestions) ?  <h4 className={this.props.suggestions.loading ? "red" : "green"}>redux suggestion list </h4> : <h4>redux suggestion list </h4> 
+        
+        // let title = (this.props.suggestions) ?  <h4 className={this.props.suggestions.loading ? "red" : "green"}>Suggestion list with Redux  </h4> : <h4>Suggestion list with Redux  </h4> 
        
         return (
         <div className="row">
                 <div className="col-sm-6">    
-                <h4>redux suggestion list </h4>     
+                    <h4>Suggestion list with Redux</h4>
                 <KeywordInput />      
                 <SuggestionList tableClass="table table-borderd table-condensed table-hover" />
             </div>
--- src/reducers/reducer_active_suggestion.js
+++ src/reducers/reducer_active_suggestion.js
@@ -1,5 +1,4 @@
-// state argument is not applicatoin state, only the state this reducer is responsible for
-
+// Get the current selected suggestion.  This could be from the suggestion list or input field
 export const ActiveSuggestionReducer = (state = '', action) => {
     switch (action.type) {
         case 'SUGGESTION_SELECTED':
